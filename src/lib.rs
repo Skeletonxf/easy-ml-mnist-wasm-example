1264c65dd3a4c74db4e7a7a3886c7e4f90432e7b
Skeletonxf <skeletonxf@gmail.com>
2020-09-04 22:52:40 +0100

    Optimise for speed instead of file size

--- src/lib.rs
+++ src/lib.rs
@@ -21,11 +21,11 @@ macro_rules! log {
     }
 }
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+// // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// // allocator.
+// #[cfg(feature = "wee_alloc")]
+// #[global_allocator]
+// static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
 extern {
